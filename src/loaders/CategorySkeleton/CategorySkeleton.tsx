401bd2116f77c939274abe545117dd3da8743b85
dmromaniv <romanivdmytro@gmail.com>
2025-01-10 15:31:11 +0200

    Added key value fol skeletons

--- src/loaders/CategorySkeleton/CategorySkeleton.tsx
+++ src/loaders/CategorySkeleton/CategorySkeleton.tsx
@@ -3,8 +3,13 @@ import Skeleton from "react-loading-skeleton";
 const CategorySkeleton = () => {
   return (
     <div className="grid gap-4 max-xl:px-5 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 xl:text-center">
-      {Array.from({ length: 5 }).map(() => (
-        <Skeleton height={30} highlightColor="#c7d8fe" baseColor="#EBF2FF" />
+      {Array.from({ length: 5 }).map((_, index) => (
+        <Skeleton
+          key={index}
+          height={30}
+          highlightColor="#c7d8fe"
+          baseColor="#EBF2FF"
+        />
       ))}
     </div>
   );
--- src/loaders/ProductCardSkeleton/ProductCardSkeleton.tsx
+++ src/loaders/ProductCardSkeleton/ProductCardSkeleton.tsx
@@ -3,8 +3,13 @@ import Skeleton from "react-loading-skeleton";
 const ProductCardSkeleton = () => {
   return (
     <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5">
-      {Array.from({ length: 10 }).map(() => (
-        <Skeleton height={300} highlightColor="#c7d8fe" baseColor="#EBF2FF" />
+      {Array.from({ length: 10 }).map((_, index) => (
+        <Skeleton
+          key={index}
+          height={300}
+          highlightColor="#c7d8fe"
+          baseColor="#EBF2FF"
+        />
       ))}
     </div>
   );
